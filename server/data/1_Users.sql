573adb63db938746054ca2f7c88f3c8ae715ca29
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-10-12 00:45:51 +0800

    Add table, some user, caretakers and categories pre data

---
+++ server/data/1_Users.sql
@@ -0,0 +1,32 @@
+INSERT INTO Users (
+        username,
+        password,
+        contact_number,
+        name,
+        is_pcs_admin,
+        is_pet_owner
+    )
+VALUES (
+        'josiahkhoo',
+        'FxYyHwdoJZ1',
+        '98669989',
+        'Josiah Khoo',
+        True,
+        True
+    );
+INSERT INTO Users (
+        username,
+        password,
+        contact_number,
+        name,
+        is_pcs_admin,
+        is_pet_owner
+    )
+VALUES (
+        'yumingchen',
+        '?aZH3k7:A;uv_#!{',
+        '96417023',
+        'Chen Yu Ming',
+        True,
+        True
+    );
