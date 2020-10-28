095f9477407687123f83a9d957321f6b70297245
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2020-10-28 16:25:35 +0800

    Add required queries

--- server/README.md
+++ server/README.md
@@ -28,4 +28,59 @@
     "total_earnings": "1100",
     "post_60_days_earnings": null,
     "is_full_time": true
+    "salary": 1000.0
 }
+
+## Availablility
+- Can sort by any number of these params below
+- Endpoint: "/available/"
+- GET `localhost:5000/available/?name=&start=2020-10-01&end=2020-10-31&category=Do&price=100&rating=0`
+- Possible Params to sort by {name, start, end, category, price ,rating}` - availablilty under parameters given
+- DATE should be in `YYYY/MM/DD`
+Example:
+- Query Params Inputs on postman: {
+        "name": null
+        "start": "2020-10-01"
+        "end": "2020-10-31"
+        "price": 100,
+        "category": "Do"
+        "rating": 3.6
+  }
+- Availablity: [
+    {
+        "userid": 2,
+        "named": "Josiah Khoo",
+        "contact": "98669989",
+        "price": 100,
+        "category": "Dogs",
+        "round": "4.00"
+    }
+]
+
+Caretakers Salary:
+- Endpoint: "/caretakers/salary/"
+- Optional Parameters Start/End (default is full range of dates)
+- GET `localhost:5000/caretakers/salary?start=2020-10-01&end=2020-10-31`
+- Possible Params to set {start, end}` - availablilty under parameters given
+- Query Params Inputs on postman: {
+        "start": "2020-10-01"
+        "end": "2020-10-31"
+  }
+- Expected Salary: [
+        {
+            "care_taker_user_id": 2,
+            "pet_day": "7",
+            "total_earnings": "21099",
+            "post_60_days_earnings": null,
+            "is_full_time": false,
+            "salary": "10549.500"
+        },
+        {
+            "care_taker_user_id": 3,
+            "pet_day": "4",
+            "total_earnings": "3300",
+            "post_60_days_earnings": null,
+            "is_full_time": true,
+            "salary": "1000"
+        }
+    ]
--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.get("/admin/:workdays", async (req, res) => {
 });
 
 //insert into ownedpets
-app.put("/pets/:user_id:catname:petname", async (req, res) => {
+app.put("/pets/:user_id/:catname/:petname", async (req, res) => {
   try{
     const { user_id, catname, petname } = req.params;
     console.log(user_id, catname, petname)
@@ -70,81 +70,105 @@ app.put("/pets/:user_id:catname:petname", async (req, res) => {
   }
 });
 
-// get available dates of between a given interval
-app.get("/available/:start:end", async (req, res) => {
-  try{
-    const { start, end } = req.params;
-    console.log(start, end)
-    const findAvailableInterval = await pool.query(
-    "SELECT u.user_id as userid," +
-    "u.name as name, " +
-    "u.contact_number as contact," +
-    "c.daily_price as price" +
-    "FROM isAvailableOn a " +
-    "INNER JOIN Users u ON a.care_taker_user_id = u.user_id " +
-    "INNER JOIN CanTakeCare c ON a.care_taker_user_id = c.care_taker_user_id" +
-    "WHERE (a.available_date > $1 AND a.available_date <= $2)",[start, end]
-    );
-    console.log(findAvailableInterval);
-    res.json(findAvailableInterval.rows);
-  } catch (error) {
-    res.status(500).json("Error");
-    console.error(error.message);
-  }
-});
-// get combined available caretakers -> daterange, name, pet category
-app.get("/available/:name/:start/:end/:category/:rating", async (req, res) => {
+//get expected salary within a certain date range(expecting a month range)
+app.get("/caretakers/salary/", async (req, res) => {
   try {
-    const { name ,start, end, category, rating } = req.params
-    console.log( name ,start, end, category, rating)
-    // not sure how to validate inputs
-    const findAvaliable = await pool.query(
-      "SELECT x.* , ROUND(AVG(y.avg_rating), 2) "+
-      "FROM (SELECT DISTINCT u.user_id as userid," +
-      "u.name as named," +
-      "u.contact_number as contact," +
-      "c.daily_price as price" +
-      "FROM isAvailableOn a " +
-      "JOIN Users u ON a.care_taker_user_id = u.user_id" +
-      "JOIN CanTakeCare c ON a.care_taker_user_id = c.care_taker_user_id " +
-      "WHERE c.category_name LIKE COALESCE(CAST($1 as VARCHAR), '')||'%' " +
-      "AND a.available_date > COALESCE(CAST($2 AS DATE), DATE('1970-01-01')) " +
-      "AND a.available_date <= COALESCE(CAST($3 AS DATE), '9999-12-31') " +
-      "AND u.name LIKE COALESCE(CAST($4 as VARCHAR), '')||'%') as x " +
-      "LEFT OUTER JOIN (SELECT u.user_id as userid, " +
-      "u.name as named, u.contact_number as contact," +
-      "ROUND(AVG(b.rating), 2) as avg_rating" +
-      "FROM Bid b INNER JOIN Users u ON b.care_taker_user_id = u.user_id" +
-      "GROUP BY (u.user_id, b.care_taker_user_id, u.name, u.contact_number)) as y ON" +
-      "x.userid = y.userid " +
-      "GROUP BY (x.userid, x.named, x.contact, x.price, y.userid, y.named, y.contact)" +
-      "HAVING AVG(y.avg_rating) >= COALESCE(CAST($5 AS NUMERIC), 0) OR AVG(y.avg_rating) IS NULL"
-      , [category, start, end, name, rating]
-      );
-      console.log(findAvailable);
-      res.json(findAvailable.rows);
+    let start = req.query.start
+    let end = req.query.end
+    // in order to ensure sql input is null
+    start = start ? start : null
+    end = end ? end : null
+    console.log(start, end)
+    const expectedSalary = await pool.query(
+      `with RECURSIVE t as (
+        select care_taker_user_id, start_date, end_date, 1 as index, (end_date - start_date + 1) as date_count, 
+      (total_price/A.date_count) as daily_price
+        from (select care_taker_user_id, start_date,end_date,(end_date - start_date + 1) as date_count, total_price from bid) as A
+        where start_date >= (SELECT COALESCE(CAST($1 as date), DATE('1970-01-01')))
+        AND start_date <= (SELECT COALESCE(CAST($2 as date), DATE('9999-12-31') ))
+          union all
+        select care_taker_user_id, start_date, end_date, index + 1, date_count, daily_price
+        from t
+        where index < date_count
+      )
+    SELECT care_taker_user_id, count(*) as pet_day, sum(t.daily_price) as total_earnings,
+       (SELECT sum(daily_price)
+      FROM (SELECT *,
+        rank() OVER (
+        PARTITION BY care_taker_user_id
+        ORDER BY care_taker_user_id, start_date, end_date, index ASC)
+        FROM t) as t2
+      WHERE rank > 60
+      AND t.care_taker_user_id = t2.care_taker_user_id
+      ) as post_60_days_earnings, caretakers.is_full_time,
+      (CASE WHEN caretakers.base_salary IS NULL AND caretakers.is_full_time = false
+          THEN sum(t.daily_price) * caretakers.commission_rate -- parttimers
+           WHEN count(*) > 60 THEN (SELECT sum(daily_price)
+      FROM (SELECT *,
+        rank() OVER (
+        PARTITION BY care_taker_user_id
+        ORDER BY care_taker_user_id, start_date, end_date, index ASC)
+        FROM t) as t2
+      WHERE rank > 60
+      AND t.care_taker_user_id = t2.care_taker_user_id
+      ) * caretakers.commission_rate + caretakers.base_salary
+       ELSE caretakers.base_salary
+       END) as salary
+    FROM t JOIN caretakers ON t.care_taker_user_id = caretakers.user_id
+    GROUP BY t.care_taker_user_id, caretakers.is_full_time, caretakers.base_salary, caretakers.commission_rate;`, [start, end]
+    );
+    res.status(200).json(expectedSalary);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json("Error");
+    res.status(500).json("Error")
   }
 });
 
-// get all availability for a given carer name
-app.get("/available/:name", async (req, res) => {
-  try{
-    const { caretakerName } = req.params;
-    console.log(caretakerName)
-    const findAvailableName = await pool.query(
-    "SELECT u.user_id as userid," +
-    "u.name as name," +
-    "u.contact_number as contact," +
-    "c.daily_price as price" +
-    "FROM isAvailableOn a " +
-    "INNER JOIN Users u ON a.care_taker_user_id = u.user_id" +
-    "INNER JOIN CanTakeCare c ON a.care_taker_user_id = c.care_taker_user_id" +
-    "WHERE u.name LIKE '$1%'", [caretakerName])
-    console.log(findAvailableName);
-    res.json(findAvailableName.rows);
+// get combined available caretakers -> daterange, name, pet category
+app.get("/available/", async (req, res) => {
+  try {
+    let name = req.query.name
+    let start = req.query.start
+    let end = req.query.end
+    let category = req.query.category
+    let price = req.query.price
+    let rating = req.query.rating
+    //validate inputs
+    name = name ? name : null
+    start = start ? start : null
+    end = end ? end : null
+    category = category ? category : null
+    price = price ? price : null
+    rating = rating ? rating : null
+    console.log( name ,start, end, category, rating)
+    const findAvailable = await pool.query(
+      `SELECT DISTINCT x.* , ROUND(AVG(y.avg_rating), 2)
+      FROM (SELECT DISTINCT u.user_id as userid,
+      u.name as named,
+      u.contact_number as contact,
+      c.daily_price as price,
+      c.category_name as category
+      FROM isAvailableOn a
+      JOIN Users u ON a.care_taker_user_id = u.user_id
+      JOIN CanTakeCare c ON a.care_taker_user_id = c.care_taker_user_id
+      WHERE c.category_name LIKE COALESCE(CAST($1 as VARCHAR), '')||'%'
+      AND a.available_date > COALESCE(CAST($2 AS DATE), DATE('1970-01-01'))
+      AND a.available_date <= COALESCE(CAST($3 AS DATE), '9999-12-31')
+      AND u.name LIKE COALESCE(CAST($4 as VARCHAR), '')||'%' 
+      AND c.daily_price <= COALESCE(CAST($5 as INT), 20000)
+         ) as x
+      LEFT OUTER JOIN (SELECT u.user_id as userid,
+      u.name as named, u.contact_number as contact,
+      ROUND(AVG(b.rating), 2) as avg_rating
+      FROM Bid b INNER JOIN Users u ON b.care_taker_user_id = u.user_id
+      GROUP BY (u.user_id, b.care_taker_user_id, u.name, u.contact_number)) as y ON
+      x.userid = y.userid 
+      GROUP BY (x.userid, x.named, x.contact, x.price, y.userid, y.named, y.contact, x.category)
+      HAVING AVG(y.avg_rating) >= COALESCE(CAST($6 AS NUMERIC), 0) OR AVG(y.avg_rating) IS NULL`
+      , [category, start, end, name, price ,rating]
+      );
+      // console.log(findAvailable);
+      res.status(200).json(findAvailable.rows);
   } catch (error) {
     console.error(error.message);
     res.status(500).json("Error");
@@ -156,9 +180,9 @@ app.get("/test/:petType", async (req, res) => {
   try {
     const { petType } = req.params;
     const avgRating = await pool.query(
-        "SELECT DISTINCT u.user_id as userid, u.name as name, u.contact_number as contact, c.daily_price as price FROM " +
-        "isAvailableOn a INNER JOIN Users u ON a.care_taker_user_id = u.user_id INNER JOIN CanTakeCare c " +
-        "ON a.care_taker_user_id = c.care_taker_user_id WHERE c.category_name = $1", [petType]
+        `SELECT DISTINCT u.user_id as userid, u.name as name, u.contact_number as contact, c.daily_price as price FROM 
+        isAvailableOn a INNER JOIN Users u ON a.care_taker_user_id = u.user_id INNER JOIN CanTakeCare c 
+        ON a.care_taker_user_id = c.care_taker_user_id WHERE c.category_name = $1`, [petType]
     );
     if (avgRating.rows.length) {
       res.status(200).json(avgRating.rows);
@@ -171,6 +195,8 @@ app.get("/test/:petType", async (req, res) => {
     console.error(error.message);
   }
 });
+
+
 
 // get user by id
 app.get("/users/:id", async (req, res) => {
