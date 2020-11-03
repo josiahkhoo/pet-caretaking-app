06c5e2d609cc167c093ae0025e68f4efe25f26b1
Josiah Khoo <josiahkhooshaoqi@gmail.com>
2020-11-04 00:16:31 +0800

    Fix login user_id being null

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -79,7 +79,7 @@ module.exports = {
     try {
       const { username, password } = req.body;
       const user = await pool.query(
-        "SELECT * FROM users LEFT JOIN caretakers ON users.user_id = caretakers.user_id WHERE username=$1 AND password=$2",
+        "SELECT users.user_id AS user_id, username, password, contact_number, name, is_pcs_admin, is_pet_owner, image_url, address, bonus_rate, base_salary, minimum_base_quota, commission_rate, is_full_time FROM users LEFT JOIN caretakers ON users.user_id = caretakers.user_id WHERE username=$1 AND password=$2",
         [username, password]
       );
       if (user.rows.length == 1) {
