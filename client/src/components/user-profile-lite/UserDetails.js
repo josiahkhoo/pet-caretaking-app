7cd6209d95e23bf24eb0ed6d29e55c46edcb78df
lalalawson <lawson.tkw@gmail.com>
2020-11-03 21:51:16 +0800

    added temporary comments to div photo url in user profile, updated satisfaction for null values to N.A., updated server API calls to include year and caretaker name

--- client/src/components/user-profile-lite/UserDetails.js
+++ client/src/components/user-profile-lite/UserDetails.js
@@ -12,15 +12,15 @@ import {
 const UserDetails = ({ user }) => (
   <Card small className="mb-4 pt-3">
     <CardHeader className="border-bottom text-center">
-      <div className="mb-3 mx-auto">
+      {/* {<div className="mb-3 mx-auto">
         <img
           className="rounded-circle"
           src={user.image_url}
           alt={user.name}
           width="200"
         />
-      </div>
-      <h4 className="mb-0">{user.name}</h4>
+      </div>}
+      <h4 className="mb-0">{user.name}</h4> */}
       <Button pill outline size="sm" className="mb-2">
         <i className="material-icons mr-1">person_add</i>
       </Button>
