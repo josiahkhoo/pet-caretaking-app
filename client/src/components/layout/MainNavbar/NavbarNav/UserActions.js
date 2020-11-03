79c398d262299257cf81a8a986d84077f637358d
lalalawson <lawson.tkw@gmail.com>
2020-11-03 22:53:21 +0800

    routed top right user actions to user profile

--- client/src/components/layout/MainNavbar/NavbarNav/UserActions.js
+++ client/src/components/layout/MainNavbar/NavbarNav/UserActions.js
@@ -46,10 +46,10 @@ export default class UserActions extends React.Component {
           <span className="d-none d-md-inline-block">{user.name}</span>
         </DropdownToggle>
         <Collapse tag={DropdownMenu} right small open={this.state.visible}>
-          <DropdownItem tag={Link} to="user-profile">
+          <DropdownItem tag={Link} to="user-profile-lite">
             <i className="material-icons">&#xE7FD;</i> Profile
           </DropdownItem>
-          <DropdownItem tag={Link} to="edit-user-profile">
+          <DropdownItem tag={Link} to="user-profile-lite">
             <i className="material-icons">&#xE8B8;</i> Edit Profile
           </DropdownItem>
           <DropdownItem divider />
