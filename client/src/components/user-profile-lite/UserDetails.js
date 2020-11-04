61d1e90f4b059c7c00b9c3ce0535f3ce4f59b8e0
lalalawson <lawson.tkw@gmail.com>
2020-11-05 02:46:49 +0800

    updated userdetails to show image_url in profile and top nav bar

--- client/src/components/user-profile-lite/UserDetails.js
+++ client/src/components/user-profile-lite/UserDetails.js
@@ -12,7 +12,7 @@ import {
 const UserDetails = ({ user }) => (
   <Card small className="mb-4 pt-3">
     <CardHeader className="border-bottom text-center">
-      {/* {<div className="mb-3 mx-auto">
+      {<div className="mb-3 mx-auto">
         <img
           className="rounded-circle"
           src={user.image_url}
@@ -20,7 +20,7 @@ const UserDetails = ({ user }) => (
           width="200"
         />
       </div>}
-      <h4 className="mb-0">{user.name}</h4> */}
+      <h4 className="mb-0">{user.name}</h4>
       <Button pill outline size="sm" className="mb-2">
         <i className="material-icons mr-1">person_add</i>
       </Button>
