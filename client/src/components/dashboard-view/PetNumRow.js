9eb80e4e4a468a2027eb1070a78d60dabf43623b
lalalawson <lawson.tkw@gmail.com>
2020-11-05 03:38:07 +0800

    readded as PetNumRow.js

---
+++ client/src/components/dashboard-view/PetNumRow.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import monthName from "../../utils/monthName";
+
+export default class PetNumRow extends Component {
+  render() {
+    const { row } = this.props;
+    return (
+      <tr key={row}>
+        <td>{monthName(row.month)}</td>
+        <td>{row.year}</td>
+        <td>{row.count}</td>
+      </tr>
+    );
+  }
+}
