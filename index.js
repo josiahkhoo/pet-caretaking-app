04728bafc64ca73f00e7bd42f0ba70fce7c41fad
Calvin <cxingzhu@hotmail.com>
2020-11-06 23:49:29 +0800

    add index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path")
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
@@ -18,7 +19,7 @@ const { viewReviews } = require("./controllers/PetOwnerController");
 const PetOwnerController = require("./controllers/PetOwnerController");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 }
 // Routes
 
@@ -250,4 +251,8 @@ app.get("/categories", async (req, res) => {
 
 app.listen(PORT, () => {
   console.log("server listening at ", PORT);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
