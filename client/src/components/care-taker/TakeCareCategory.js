164fe8075c300c4269179d3d6d84fc14d981d0ef
Calvin <cxingzhu@hotmail.com>
2020-11-14 18:07:04 +0800

    show can takecare

--- client/src/components/care-taker/TakeCareCategory.js
+++ client/src/components/care-taker/TakeCareCategory.js
@@ -24,8 +24,6 @@ const TakeCareCategory = ({ user }) => {
   
   const category = ["Dogs","Cats","Rabbits","Hamsters","Birds"]
   const [data, setData] = useState([])
-  const [dog, setDog] = useState([])
-  const [cat, setCat] = useState([])
 
   useEffect(async () => {
     try {
@@ -35,12 +33,7 @@ const TakeCareCategory = ({ user }) => {
 
         return response.json()
       })
-      setData(result.map(item => [item.category_name, item.daily_price])).then(() =>
-        {
-          setDog(data.filter(item => item[0] == category[0]));
-          setCat(data.filter(item => item[0] == category[1]));
-        }
-      )
+      setData(result)
 
     } catch (error) {
       console.log(error);
@@ -51,9 +44,8 @@ const TakeCareCategory = ({ user }) => {
     if (data.length == 0) {
       return 0
     }
-    const result = data.filter(x => x[0] == name)
-    console.log(name, result[1], result.length)
-    return result.length == 1 ? result[1] : 0
+    const result = data.filter(x => x.category_name == name)
+    return result.length == 1 ? result.daily_price : 0
   }
 
   const updateName = (name) => {
@@ -61,7 +53,7 @@ const TakeCareCategory = ({ user }) => {
       console.log(data)
       setData(data.push([name, 0]))
     } else {
-      setData(data.filter(x => x[0] != name))
+      setData(data.filter(x => x.category_name != name))
     }
   }
 
@@ -73,7 +65,7 @@ const TakeCareCategory = ({ user }) => {
     if (!containsName(name)) {
       setData(data.push([name, price]))
     } else {
-      setData(data.map(x => x[0] == name ? [name, price] : x))
+      setData(data.map(x => x.category_name == name ? [name, price] : x))
     }
     console.log(data)
   }
@@ -81,108 +73,66 @@ const TakeCareCategory = ({ user }) => {
   const containsName = (name) => {
     return data.filter(x => x[0] == name).length > 0;
   }
-  console.log(dog)
   console.log("data ", data)
 
-
-  if (data == []) return null;
+  if (data.length == 0) return null;
   return (
       <Card>
       <CardHeader className="border-bottom">
-        <h6 className="m-0">Pet Categories To Take Care</h6>
+        <h6 className="m-0">Pet Category Pricing</h6>
       </CardHeader>
       <CardBody>
-        <Row>
-          <Col lg="2">
-            <FormCheckbox
-              className="mt-2"
-              defaultChecked={dog.length != 0}
-              name="a"
-              value={category[0]}
-              onChange={(e) => {
-                if (dog != []) {
-                  setDog([])
-                } else {
-                  setDog([category[0], 0])
-                }
-                console.log(dog)
-                console.log(data)
-              }}
-            >
-              {category[0]}
-            </FormCheckbox>
-          </Col>
-          <Col lg="3">
+      <Row form>
+        <Col md="3" className="form-group">
+          <label htmlFor="feEmail">Category</label>
+        </Col>
+        <Col md="3" className="form-group">
+          <label htmlFor="feEmail">Daily Price</label>
+        </Col>
+      </Row>
+        {
+          data.map(item => 
+            <Row form >
+              <Col md="3" className="form-group">
+                <FormInput
+                  type="text"
+                  key={item.category_name}
+                  value={item.category_name}
+                  disabled
+                />
+              </Col>
+              <Col md="3" className="form-group">
+                <FormInput
+                  type="text"
+                  key={item.daily_price}
+                  value={"$".concat(item.daily_price)}
+                  disabled
+                />
+              </Col>
+            </Row>
+          )
+        }
+        {/* <Row form >
+          <Col md="3" className="form-group">
             <FormInput
-              name="a"
-              value={getPrice(category[0])}
-              disabled={dog.length == 0}
-              type="number"
-              maxLength="8"
-              onChange={(e) => {
-                console.log(dog)
-                console.log(data)
-              }}
+              type="text"
+              key={item.category_name}
+              value={item.category_name}
+              disabled
             />
           </Col>
-        </Row>
-
-        <Row>
-          <Col lg="2">
-            <FormCheckbox
-              className="mt-2"
-              name="a"
-              value={category[1]}
-              defaultChecked={cat.length != 0}
-              onChange={(e) => {
-                updateName(e.target.value)
-                console.log(data)
-              }}
-            >
-              {category[1]}
-            </FormCheckbox>
-          </Col>
-          <Col lg="3">
+          <Col md="3" className="form-group">
             <FormInput
-              name={category[1]}
-              value={getPrice(category[1])}
-              disabled={!containsName(category[1])}
-              type="number"
-              maxLength="8"
-              onChange={(e) => updatePrice(e.target.name, parseFloat(e.target.value))}
+              type="text"
+              key={item.daily_price}
+              value={"$".concat(item.daily_price)}
+              disabled
             />
           </Col>
-        </Row>
-
-        <Row>
-          <Col lg="2">
-            <FormCheckbox
-              className="mt-2"
-              name={category[2]}
-              value={category[2]}
-              onChange={(e) => {
-                updateName(e.target.value)
-                console.log(data)
-              }}
-            >
-              {category[2]}
-            </FormCheckbox>
-          </Col>
-          <Col lg="3">
-            <FormInput
-              name={category[2]}
-              value={getPrice(category[2])}
-              disabled={!containsName(category[2])}
-              type="number"
-              maxLength="8"
-              onChange={(e) => updatePrice(e.target.name, parseFloat(e.target.value))}
-            />
-          </Col>
-        </Row>
-
+        </Row> */}
       </CardBody>
-      <CardFooter className="mb-5">
-        <Button className="mr-2" onClick={update}>Update</Button>
+      <CardFooter className="mb-2">
+        {/* <Button className="mr-2" onClick={update}>Update</Button> */}
         </CardFooter>
     </Card>
 )};
